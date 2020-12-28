bd2c7808b1f279d9562fb771b4059f5b18f2e491
pauloalchemist <paulolinsdev@gmail.com>
2020-12-28 16:58:35 -0300

    return 500 if an invalid httpRequest is provide

---
+++ tests/routes/login-router.spec.js
@@ -0,0 +1,52 @@
+class LoginRouter {
+  route (httpRequest) {
+    if (!httpRequest || !httpRequest.body) {
+      return {
+        statusCode: 500
+      }
+    }
+    const { email, password } = httpRequest.body
+    if (!email || !password) {
+      return {
+        statusCode: 400
+      }
+    }
+  }
+}
+
+describe('Login Router', () => {
+  test('Should return 400 if no email is provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        password: '123456'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('Should return 400 if no password is provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: 'hadduken@shoryuken.com'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('Should return 500 if no httpRequest is provided', () => {
+    const sut = new LoginRouter()
+    const httpResponse = sut.route()
+    expect(httpResponse.statusCode).toBe(500)
+  })
+
+  test('Should return 500 if httpRequest no body', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {}
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+  })
+})
