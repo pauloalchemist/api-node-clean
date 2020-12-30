bc2eb12ddd0c5141d468535ec0508d5626e4ccde
pauloalchemist <paulolinsdev@gmail.com>
2020-12-30 18:45:51 -0300

    http-response param errpr in badRequest

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -1,4 +1,5 @@
 const httpResponse = require('../helpers/http-response')
+const MissingParamError = require('../helpers/missing-param-error')
 
 module.exports = class LoginRouter {
   constructor (authUseCase) {
@@ -9,11 +10,11 @@ module.exports = class LoginRouter {
     try {
       const { email, password } = httpRequest.body
       if (!email) {
-        return httpResponse.badRequest('email')
+        return httpResponse.badRequest(new MissingParamError('email'))
       }
 
       if (!password) {
-        return httpResponse.badRequest('password')
+        return httpResponse.badRequest(new MissingParamError('password'))
       }
       const accessToken = await this.authUseCase.auth(email, password)
       if (!accessToken) {
