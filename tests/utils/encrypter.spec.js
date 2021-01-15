9c208af5ef1d37cd8b8e464b2b66baaed587a1c4
pauloalchemist <paulolinsdev@gmail.com>
2021-01-14 22:01:21 -0300

    move sut creation to a factory helper method

--- tests/utils/encrypter.spec.js
+++ tests/utils/encrypter.spec.js
@@ -7,22 +7,26 @@ class Encrypter {
   }
 }
 
+const makeSut = () => {
+  return new Encrypter()
+}
+
 describe('Encrypter', () => {
   test('Should return true if bcrypt returns true', async () => {
-    const sut = new Encrypter()
+    const sut = makeSut()
     const isValid = await sut.compare('qualquervalor', 'hashed_value')
     expect(isValid).toBe(true)
   })
 
   test('Should return true if bcrypt returns true', async () => {
-    const sut = new Encrypter()
+    const sut = makeSut()
     bcrypt.isValid = false
     const isValid = await sut.compare('qualquervalor', 'hashed_value')
     expect(isValid).toBe(false)
   })
 
   test('Should call bcrypt with correct values', async () => {
-    const sut = new Encrypter()
+    const sut = makeSut()
     await sut.compare('qualquervalor', 'hashed_value')
     expect(bcrypt.value).toBe('qualquervalor')
     expect(bcrypt.hash).toBe('hashed_value')
