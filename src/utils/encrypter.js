1078a8f4c23f320488a1047023bb62b2c30c5644
pauloalchemist <paulolinsdev@gmail.com>
2021-01-14 22:16:56 -0300

    ensure Encrypter throws if no params are provided

--- src/utils/encrypter.js
+++ src/utils/encrypter.js
@@ -1,7 +1,14 @@
 const bcrypt = require('bcrypt')
+const { MissingParamError } = require('./errors-generics')
 
 module.exports = class Encrypter {
   async compare (value, hash) {
+    if (!value) {
+      throw new MissingParamError('value')
+    }
+    if (!hash) {
+      throw new MissingParamError('hash')
+    }
     const isValid = bcrypt.compare(value, hash)
     return isValid
   }
--- tests/utils/encrypter.spec.js
+++ tests/utils/encrypter.spec.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt')
 const Encrypter = require('../../src/utils/encrypter')
+const { MissingParamError } = require('../../src/utils/errors-generics')
 
 const makeSut = () => {
   return new Encrypter()
@@ -24,5 +25,13 @@ describe('Encrypter', () => {
     await sut.compare('qualquervalor', 'hashed_value')
     expect(bcrypt.value).toBe('qualquervalor')
     expect(bcrypt.hash).toBe('hashed_value')
+  })
+
+  test('Should throw if no params are provided', async () => {
+    const sut = makeSut()
+    expect(sut.compare()).rejects.toThrow(new MissingParamError('value'))
+    expect(sut.compare('qualquervalor')).rejects.toThrow(
+      new MissingParamError('hash')
+    )
   })
 })
