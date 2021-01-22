fdbe7417cdc5ac8cb70eac1483036c77b6bdf5d1
pauloalchemist <paulolinsdev@gmail.com>
2021-01-22 19:27:24 -0300

    improved port server

--- src/main/index.js
+++ src/main/index.js
@@ -1,8 +1,11 @@
 const MongoHelper = require('../infra/helpers/mongo-helper')
 const env = require('./config/env')
+
 MongoHelper.connect(env.mongoUrl)
   .then(() => {
     const app = require('./config/app')
-    app.listen(5858, () => console.log('Server Running'))
+    app.listen(env.port, () =>
+      console.log(`Server running at http://localhost:${env.port}`)
+    )
   })
   .catch(console.error)
